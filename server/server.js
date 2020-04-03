a2f2252c4365f1ec91e252066a572099c720e773
Grant <gdons849@mtroyal.ca>
2020-04-03 17:09:04 -0600

    Authentication Function Added

--- server/server.js
+++ server/server.js
@@ -1,10 +1,70 @@
 const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
+const parser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const flash = require('express-flash');
 
+// use .env file for configuration constants
+require('dotenv').config();
+
+// create connection to database
+require('./handlers/dataConnector.js').connect();
+
+// create an express app
 const app = express();
 
 const movieRoutes = require("./routes/movie-routes");
 const userRoutes = require("./routes/user-routes");
 const HttpError = require("./models/http-error");
+
+// view engine setup
+app.use(expressLayouts);
+app.set('view engine', 'ejs'); 
+
+// serves up static files from the public folder. 
+app.use(express.static('public'));
+app.use('/static', express.static('public'));
+
+// setup express middleware
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+
+// Express session
+app.use(cookieParser('oreos'));
+app.use(
+    session({
+      secret: process.env.SECRET,
+      resave: true,
+      saveUninitialized: true
+    })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+// Use express flash
+app.use(flash());
+// set up the passport authentication
+require('./handlers/auth.js');
+// set up route handlers
+const openRoutes = require('./handlers/openRouter.js');
+app.use('/', openRoutes);
+
+// defined routes
+app.get("/", (req, res) => res.send("API Running"));
+app.use("/api/movies", movieRoutes);
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ error : err });
+});
+
+// error handling for unsupported routes
+app.use((req, res, next) => {
+  return next(new HttpError("Could not find this route.", 404));
+});
 
 // dont need the json body parser anymore. built into express
 app.use(express.json({ extended: false }));
@@ -21,13 +81,8 @@ app.use((req, res, next) => {
   next();
 });
 
-// defined routes
-app.get("/", (req, res) => res.send("API Running"));
-app.use("/api/movies", movieRoutes);
-
-// error handling for unsupported routes
-app.use((req, res, next) => {
-  return next(new HttpError("Could not find this route.", 404));
+// Use express to listen to port
+let port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log("Server now running at port= " + port);
 });
-
-app.listen(5000);
