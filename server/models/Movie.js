b3fbfc2b4558f0640000e783e9364632621ce6d7
mannka <kmann1511@gmail.com>
2020-04-04 09:35:13 -0600

    Update Movie.js

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,36 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const movieSchema = new.mongoose.Schema({
-    id: Number,
-    tmdb_id: Number,
-    imdb_id: String,
-    release_date: String,
-    title: String,
-    runtime: Number,
-    revenue: Number,
-    tagline: String,
-    poster: String,
-    ratings:{
-        popularity: Number,
-        average: Number,
-        count: Number
-    },
-    details:{
-        overview: String,
-        genres:[
-            {
-                id: Number,
-                name: String
-            },
-            {
-                id: Number,
-                name: String
-            },
-            {
-                id: Number,
-                name: String
-            }
-        ]
-    }
-})
-module.exports = mongoose.model('Movie', movieSchema);
+const movieSchema = new mongoose.Schema({
+  id: Number,
+  tmdb_id: Number,
+  imdb_id: String,
+  release_date: String,
+  title: String,
+  runtime: Number,
+  revenue: Number,
+  tagline: String,
+  poster: String,
+  ratings: {
+    popularity: Number,
+    average: Number,
+    count: Number
+  },
+  details: {
+    overview: String,
+    genres: [
+      {
+        id: Number,
+        name: String
+      },
+      {
+        id: Number,
+        name: String
+      },
+      {
+        id: Number,
+        name: String
+      }
+    ]
+  }
+});
+module.exports = mongoose.model("Movie", movieSchema);
