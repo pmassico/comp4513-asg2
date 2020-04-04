bf17fbbe5d92297330512d178073c7c6bd740c97
mannka <kmann1511@gmail.com>
2020-04-03 19:07:11 -0600

    merge working

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const expressLayouts = require('express-ejs-layouts');
-const parser = require('body-parser');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const passport = require('passport');
-const flash = require('express-flash');
+const expressLayouts = require("express-ejs-layouts");
+const parser = require("body-parser");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const flash = require("express-flash");
 
 // use .env file for configuration constants
-require('dotenv').config();
+require("dotenv").config();
 
 // create connection to database
-require('./handlers/dataConnector.js').connect();
+require("./handlers/dataConnector.js").connect();
 
 // create an express app
 const app = express();
@@ -24,24 +24,24 @@ const HttpError = require("./models/http-error");
 
 // view engine setup
 app.use(expressLayouts);
-app.set('view engine', 'ejs'); 
+app.set("view engine", "ejs");
 
-// serves up static files from the public folder. 
-app.use(express.static('public'));
-app.use('/static', express.static('public'));
+// serves up static files from the public folder.
+app.use(express.static("public"));
+app.use("/static", express.static("public"));
 
 // setup express middleware
 app.use(parser.json());
-app.use(parser.urlencoded({extended: true}));
+app.use(parser.urlencoded({ extended: true }));
 
 // Express session
-app.use(cookieParser('oreos'));
+app.use(cookieParser("oreos"));
 app.use(
-    session({
-      secret: process.env.SECRET,
-      resave: true,
-      saveUninitialized: true
-    })
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
 );
 
 // Passport middleware
@@ -50,23 +50,14 @@ app.use(passport.session());
 // Use express flash
 app.use(flash());
 // set up the passport authentication
-require('./handlers/auth.js');
+require("./handlers/auth.js");
 // set up route handlers
-const openRoutes = require('./handlers/openRouter.js');
-app.use('/', openRoutes);
-
-// defined routes
-app.get("/", (req, res) => res.send("API Running"));
-app.use("/api/movies", movieRoutes);
+const openRoutes = require("./handlers/openRouter.js");
+app.use("/", openRoutes);
 
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.json({ error : err });
-});
-
-// error handling for unsupported routes
-app.use((req, res, next) => {
-  return next(new HttpError("Could not find this route.", 404));
+  res.json({ error: err });
 });
 
 // dont need the json body parser anymore. built into express
@@ -99,6 +90,6 @@ app.use((req, res, next) => {
 
 // Use express to listen to port
 let port = process.env.PORT || 5000;
-app.listen(port, function () {
-    console.log("Server now running at port= " + port);
+app.listen(port, function() {
+  console.log("Server now running at port= " + port);
 });
