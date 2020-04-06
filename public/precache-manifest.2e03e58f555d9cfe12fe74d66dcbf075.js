5a794846b1fc20911da52e747522af89342be8e6
Grant <57509066+gdonst@users.noreply.github.com>
2020-04-06 13:43:12 -0600

    Folder Structure Fixes
    Also, fixed authentication routing

---
+++ public/precache-manifest.2e03e58f555d9cfe12fe74d66dcbf075.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69d11731e2a60255f42d6ac0459a67e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d90174d41159216e725",
+    "url": "/static/css/main.ba907733.chunk.css"
+  },
+  {
+    "revision": "51bb1dbe4b02877f195a",
+    "url": "/static/js/2.3659ad8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3659ad8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d90174d41159216e725",
+    "url": "/static/js/main.51132eba.chunk.js"
+  },
+  {
+    "revision": "6cd0df6eb12ca9b8a4ef",
+    "url": "/static/js/runtime-main.ea4b9cda.js"
+  },
+  {
+    "revision": "69bb1e40130b516a19861eef13a4af8c",
+    "url": "/static/media/splash.69bb1e40.jpg"
+  }
+]);
