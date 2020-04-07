bbaee8e7b08c56ec23d38dfc1d5eed41e6812a34
Grant <57509066+gdonst@users.noreply.github.com>
2020-04-06 20:39:54 -0600

    Added user information passing to React

--- server/handlers/openRouter.js
+++ server/handlers/openRouter.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const helper = require('./helpers.js');
+const fs = require('fs');
 
 // Welcome Page
 router.get('/home',  helper.ensureAuthenticated, (req, resp) => {
    let reactHome =  '/' ;
-   console.log(req.user);
-   //{message: req.user} 
-   //resp.user = req.user;
+   fs.writeFile('user.json', req.user, function (err) {
+      if (err) throw err;
+      console.log('Saved user.json!');
+   });
    resp.redirect(reactHome);
 });
 
@@ -19,6 +21,12 @@ router.get('/login', (req, resp) => {
 router.get('/logout', (req, resp) => {
    req.logout();
    req.flash('info', 'You were logged out');
+   fs.unlink('user.json', function(error) {
+      if (error) {
+          throw error;
+      }
+      console.log('Deleted user.json');
+   });
    resp.render('login', {message: req.flash('info')} );
 });
 
